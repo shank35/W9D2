1e7d624090de53628c6b412875c8eea27e9c1eee
“Kelsang” <kelsangrtsering@gmail.com>
2023-03-07 14:37:44 -0500

    work on method

--- skeleton-9/src/ttt-view.js
+++ skeleton-9/src/ttt-view.js
@@ -1,36 +1,16 @@
 class View {
   constructor(game, el) {}
-
+  
   setupBoard() {
     const ul = document.createElement("ul");
-
-    const li1 = document.createElement("li");
-    li1.setAttribute("pos", li1.innerText);
-
-    const li2 = document.createElement("li");
-    li1.setAttribute("pos", li2.innerText);
-
-    const li3 = document.createElement("li");
-    li1.setAttribute("pos", li3.innerText);
-
-    const li4 = document.createElement("li");
-    li1.setAttribute("pos", li4.innerText);
-
-    const li5 = document.createElement("li");
-    li1.setAttribute("pos", li5.innerText);
-
-    const li6 = document.createElement("li");
-    li1.setAttribute("pos", li6.innerText);
-
-    const li7 = document.createElement("li");
-    li1.setAttribute("pos", li7.innerText);
-
-    const li8 = document.createElement("li");
-    li1.setAttribute("pos", li8.innerText);
-
-    const li9 = document.createElement("li");
-    li1.setAttribute("pos", li9.innerText);
-
+    
+    for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 3; j++){
+        const li = document.createElement("li");
+        li.setAttribute("pos", [i, j]);
+        ul.append(li);
+      }
+    }
   }
   
   bindEvents() {}
